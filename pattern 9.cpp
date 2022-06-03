54ac491717f5de84014ecedbaf0c2b0fa4b4049c
akashkumargolden <85020506+akashkumargolden@users.noreply.github.com>
2022-06-03 18:08:51 +0530

    this is the 8 and 9th apttern solved

---
+++ pattern 9.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	int i=1;
+	cout<<"enter the value of n" ;
+	cin>>n;
+	while(i<=n){
+		char ch='A'+i-1;
+		int j=1;
+		while(j<=n){
+		cout<<ch;
+		j++;
+	}
+		cout<<endl;
+		i++;
+	}
+}

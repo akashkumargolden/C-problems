15541c84395eca96c6337221ea17c732939db314
akashkumargolden <85020506+akashkumargolden@users.noreply.github.com>
2022-06-25 16:55:04 +0530

    this is my 20 and 21nd solved pattern

---
+++ pattern20.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"enter the value of n ";
+	cin>>n;
+	int i=1;
+	while(i<=n){
+		int j=1;
+		int k=1;
+		while(j<=i-1){
+			cout<<" ";
+			j++;
+		}
+		while(k<=n-i+1){
+			cout<<"*";
+			k++;
+		}
+		cout<<endl;
+		i++;
+		
+	}
+}
